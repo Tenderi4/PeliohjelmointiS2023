083fa9169f7137fd6255eb456fc6a9732dcbf7d0
Ari-Matti Prinkkilä <darpr001@edu.xamk.fi>
2023-09-25 14:03:59 +0300

    Made player character move
    Needed to remember this by searching online.

--- Week4Project/Assets/Scripts/PlayerController.cs
+++ Week4Project/Assets/Scripts/PlayerController.cs
@@ -2,17 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : CharacterController
+public class PlayerController : MonoBehaviour
 {
+    private CharacterController characterController;
+
+    public float Speed = 5.0f;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        characterController = GetComponent<CharacterController>(); 
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 move = new Vector3(Input.GetAxis("Horizontal"),0,Input.GetAxis("Vertical"));
+        characterController.Move(move*Time.deltaTime*Speed);
     }
 }
