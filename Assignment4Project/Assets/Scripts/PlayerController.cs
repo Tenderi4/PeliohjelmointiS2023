588946605bd1f4bc0d5f6fdc69878edc37c4fbff
Tenderi4 <71285540+Tenderi4@users.noreply.github.com>
2023-10-04 09:02:22 +0300

    More work on assignment 4

--- Assignment4Project/Assets/Scripts/PlayerController.cs
+++ Assignment4Project/Assets/Scripts/PlayerController.cs
@@ -8,8 +8,13 @@ public class PlayerController : MonoBehaviour
 
     private Animator animator;
     private Rigidbody rb;
-    public float JumpStrength = 25f;
-    private bool isGrounded = true;
+    private GroundCheck3D check;
+
+    public float gravity = -9.81f;
+    public float gravityScale = 1;
+    public float jumpForce = 5;
+
+    private float velocity;
 
     // Start is called before the first frame update
     void Start()
@@ -20,22 +25,47 @@ public class PlayerController : MonoBehaviour
         animator.SetFloat("Speed_f",1f);
 
         rb = GetComponent<Rigidbody>();
-        rb.isKinematic = false;
+        rb.isKinematic = true;
         rb.useGravity = false;
+
+        check = GetComponent<GroundCheck3D>();
     }
 
     // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && check.isGrounded)
+        {
+            animator.SetTrigger("Jump_trig");
+            velocity = jumpForce;
+            check.isGrounded = false;
+        }
+
+        if (check.isGrounded && velocity <= 0)
+        {
+            float offset = 1;
+            velocity = 0;
+            Vector3 closestPoint = check.hit.collider.ClosestPoint(transform.position);
+            Vector3 snappedPosition = new Vector3(transform.position.x, closestPoint.y + offset, transform.position.z);
+
+            transform.position = snappedPosition;
+        }
+
+    }
+
+
     void FixedUpdate()
     {
-        isGrounded = Physics.Raycast(transform.position, Vector3.down, 0.1f);
-        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
-        {
-            rb.AddForce(JumpStrength * Vector3.up,ForceMode.Impulse);
-            animator.SetTrigger("Jump_trig");
-        }
-        if (!isGrounded)
-        {
-            rb.AddForce(Vector3.down * 9,ForceMode.Acceleration);
-        }
+       velocity += (gravity * gravityScale) * Time.fixedDeltaTime;
+       float vPos = rb.position.y + velocity * Time.fixedDeltaTime;
+       rb.MovePosition(new Vector3(rb.position.x, vPos, rb.position.z));
+    }
+    void OnCollisionEnter(Collision collision)
+    {
+        check.isGrounded = true;
+    }
+    void OnCollisionExit(Collision collision)
+    {
+        check.isGrounded = false;
     }
 }
